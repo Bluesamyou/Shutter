c33dc3dc2320d4af0f31d071a6eb0f7b0ee77f2e
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-29 22:03:47 +1000

    updating JWT & bcrypt

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,11 +14,48 @@ module.exports = function(app) {
       res.json({ token: token });
     });
   });
-};
-//     var user = { id: 1, username: "Shutter", email: "shutter@gmail.com" };
 
-//     jwt.sign({ user: user}, "key", function (err, token){
-//         res.json({token: token})
-//     })
-//     })
-// }
+  var db = require("./models");
+  var bcrypt = require("bcrypt");
+  var saltRounds = 10;
+
+  //register: storing name, email and password and redirecting to home page after signup
+
+  app.post("/api/create-user", function(req, res) {
+    // var User = req.body;
+    bcrypt.hash(req.body.passwordsignup, saltRounds, function(err, hash) {
+      db.User.create({
+        name: req.body.usernamesignup,
+        email: req.body.emailsignup,
+        password: hash
+      }).then(function(data) {
+        if (data) {
+          res.redirect("/home");
+        }
+      });
+    });
+  });
+
+  //login page: storing and comparing email and password,and redirecting to home page after login
+  app.post("/api/user", function(req, res) {
+    // var User = req.body;
+    db.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(function(user) {
+      if (!user) {
+        res.redirect("/");
+      } else {
+        bcrypt.compare(req.body.password, user.password, function(err, result) {
+          if (result == true) {
+            res.redirect("/home");
+          } else {
+            res.send("Incorrect password");
+            res.redirect("/");
+          }
+        });
+      }
+    });
+  });
+};
