63be22ed2cb49ab4184463709cded0407288d74d
Shevi <shevids@gmail.com>
2019-09-30 07:26:59 +1000

    Completed signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,44 @@
 $(document).ready(function () {
     $(".submit-button").on("click", function (event) {
+        // Prevent default action to refresh page
         event.preventDefault();
+
+        // Fetch user information from form
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        // Checks if all fields are populated
+        if ((!username, !email, !password)) {
+            return alert("Please fill all fields");
+        }
+
+        // Posts data to server
         $.ajax({
-            mehthod: "POST",
-            url: "/api/"
-        })
+            method: "POST",
+            url: "/api/create-user",
+            data: {
+                emailsignup: email,
+                usernamesignup: username,
+                passwordsignup: password
+            }
+        }).then(function (data) {
+            if (data.success) {
+                // Fires success modal
+                Swal.fire({
+                    title: "Account Successfully Created",
+                    text: "Please login with your account details",
+                    type: "success",
+                    showCancelButton: false,
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: "Proceed"
+                }).then(function (result) {
+                    if (result.value) {
+                        // Reloads to login page
+                        location.href = "/login";
+                    }
+                });
+            }
+        });
     });
 });
