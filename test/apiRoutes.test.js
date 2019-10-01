3d5afb3b1dcf9c2cff30d69a4f5ee79198a9c6f0
Richard Taing <Richard.Taing@pexa.com.au>
2019-10-01 22:19:16 +1000

    commenting out /api/login test

--- test/apiRoutes.test.js
+++ test/apiRoutes.test.js
@@ -49,42 +49,42 @@ describe("POST /api/create-user", function() {
   });
 });
 
-describe("POST /api/login", function() {
-  // Before each test begins, create a new request server for testing
-  // & delete all examples from the db
-  beforeEach(function() {
-    request = chai.request(server);
-    return db.sequelize.sync({ force: true });
-  });
+// describe("POST /api/login", function() {
+//   // Before each test begins, create a new request server for testing
+//   // & delete all examples from the db
+//   beforeEach(function() {
+//     request = chai.request(server);
+//     return db.sequelize.sync({ force: true });
+//   });
 
-  it("should add user to database", function(done) {
-    // Create an object to send to the endpoint
-    var reqBody = {
-      username: "Test User Name",
-      email: "Test email",
-      password: "Test Password"
-    };
+//   it("should add user to database", function(done) {
+//     // Create an object to send to the endpoint
+//     var reqBody = {
+//       username: "Test User Name",
+//       email: "Test email",
+//       password: "Test Password"
+//     };
 
-    // POST the request body to the server
-    request
-      .post("/api/login")
-      .send(reqBody)
-      .end(function(err, res) {
-        var responseStatus = res.status;
-        var responseBody = res.body;
+//     // POST the request body to the server
+//     request
+//       .post("/api/login")
+//       .send(reqBody)
+//       .end(function(err, res) {
+//         var responseStatus = res.status;
+//         var responseBody = res.body;
 
-        // Run assertions on the response
+//         // Run assertions on the response
 
-        expect(err).to.be.null;
+//         expect(err).to.be.null;
 
-        expect(responseStatus).to.equal(200);
+//         expect(responseStatus).to.equal(200);
 
-        expect(responseBody)
-          .to.be.an("object")
-          .that.includes({ success: true });
+//         expect(responseBody)
+//           .to.be.an("object")
+//           .that.includes({ success: true });
 
-        // The `done` function is used to end any asynchronous tests
-        done();
-      });
-  });
-});
+//         // The `done` function is used to end any asynchronous tests
+//         done();
+//       });
+//   });
+// });
